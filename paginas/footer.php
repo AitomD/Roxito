a17d09cc01b3b538092cb562e9ec9f6159631ac9
AitomD <aitomdonatoni@gmail.com>
2025-06-09 15:04:27 -0300

    novo php

---
+++ paginas/footer.php
@@ -0,0 +1,23 @@
+<footer>
+      <div class="imgfoot">
+        <div class="textfooter">
+          <h4>Acesse nossas redes sociais</h4>
+        </div>
+        <a
+          href="https://www.instagram.com/roxitoacaiburguer/"
+          title="Instagram"
+        >
+          <i class="fab fa-instagram fa-3x me-2"></i>
+        </a>
+        <a
+          href="https://wa.me/554498819798?text=Whatssap%20do%20Roxito"
+          title="Whatssap"
+        >
+          <i class="fa-brands fa-whatsapp fa-3x"></i>
+        </a>
+        <h6>
+          Todos os direitos autorais reservados - Desenvolvido por Aitom
+          Donatoni
+        </h6>
+      </div>
+    </footer>
---
+++ paginas/formu.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+    <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="footer.css">
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+    />
+</head>
+<body>
+    <?php 
+        include 'nav.php';
+    ?>
+
+
+
+
+
+
+
+    <?php
+        include 'footer.php';
+    ?>
+</body>
+</html>
